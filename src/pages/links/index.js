110a315e452f991e6b782f8816d8aadabc015ad9
Guilherme <gdsouza.ebt@gmail.com>
2022-03-02 20:24:18 -0300

    config menu

---
+++ src/pages/links/index.js
@@ -0,0 +1,8 @@
+
+export default function Links () {
+    return (
+      <div className="app">
+        <h1>Page links</h1>
+      </div>
+    )
+  } 
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/home'
+import Links from './pages/links'
+
+function RoutesApp() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route/>    
+            </Routes> 
+        </BrowserRouter>
+    )
+}
