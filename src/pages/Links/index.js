38f08ef53733d33c4aa002fa5b4a2bb3004c59fc
Guilherme <gdsouza.ebt@gmail.com>
2022-03-06 11:59:21 -0300

    creating JSand CSS on page meus links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,42 @@
 
+import './links.css'
+import { FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
+import { Link} from 'react-router-dom'
+
 export default function Links () {
     return (
-      <div>
-        <h1>Page links</h1>
-      </div>
+      <div className='links-container'>
+
+        <div className='link-header'>
+          <Link to="/">
+          <FiArrowLeft size={38} color='fff'/>
+          </Link>
+          <h1>Meus Links</h1>
+        </div>
+
+        <div className='links-item'>
+          <button className='link'>
+            <FiLink size={18} color='#fff'/>
+            https://google.com.br
+          </button>
+
+          <button className='link-delete'>
+            <FiTrash size={24} color='#ff5454'/>
+          </button>
+        </div>
+
+        <div className='links-item'>
+          <button className='link'>
+            <FiLink size={18} color='#fff'/>
+            https://google.com.br
+          </button>
+
+          <button className='link-delete'>
+            <FiTrash size={24} color='#ff5454'/>
+          </button>
+        </div>
+
+
+       </div>
     )
   } 
